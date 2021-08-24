e2837f440450d251433333e5c7c7d2696b56b92e
yangbiwan <yangbiwan@lasoplanet.com>
2021-08-24 15:51:02 +0800

    fix: fix first onload not called bug

--- ios/RNCWebView.m
+++ ios/RNCWebView.m
@@ -64,7 +64,7 @@ static NSDictionary* customCertificatesForHost;
 @end
 #endif // TARGET_OS_OSX
 
-@interface RNCWebView () <WKUIDelegate, WKNavigationDelegate, WKScriptMessageHandler,
+@interface RNCWebView () <WKUIDelegate, WKNavigationDelegate, WKScriptMessageHandler, UIWebViewDelegate,
 #if !TARGET_OS_OSX
     UIScrollViewDelegate,
 #endif // !TARGET_OS_OSX
@@ -285,6 +285,7 @@ static NSDictionary* customCertificatesForHost;
             self->_onMessage(data);
         }
     }];
+    [self.bridge setWebViewDelegate:self];
   }
 }
 
